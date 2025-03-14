78e6ed77651a6d0b2deac67d050c13a0ab373a13
Congbang <bangpham10062002@gmail.com>
2025-03-14 13:04:08 +0700

    changechange

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -1,3 +1,9 @@
 import { createNavbar } from "./components/navbar.js";
 
 document.body.insertAdjacentHTML("afterbegin", createNavbar());
+
+const menu = document.querySelector(".menu-icon");
+menu.addEventListener("click", () => {
+  const nav = document.querySelector(".nav-menu");
+  nav.classList.toggle("active");
+});
