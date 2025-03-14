7855365aeb8d36e85c8882fb575b97e3025a4e9b
Congbang <bangpham10062002@gmail.com>
2025-03-14 20:56:06 +0700

    changechange

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -3,6 +3,11 @@ import { createNavbar } from "./components/navbar.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   document.body.insertAdjacentHTML("afterbegin", createNavbar());
+  const menu = document.querySelector(".menu-icon");
+  menu.addEventListener("click", () => {
+    const nav = document.querySelector(".nav-menu");
+    nav.classList.toggle("active");
+  });
   renderCart();
 });
 
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -47,6 +47,11 @@ function addToCart(itemId) {
 
 document.addEventListener("DOMContentLoaded", () => {
   document.body.insertAdjacentHTML("afterbegin", createNavbar());
+  const menu = document.querySelector(".menu-icon");
+  menu.addEventListener("click", () => {
+    const nav = document.querySelector(".nav-menu");
+    nav.classList.toggle("active");
+  });
   renderMenuItems(menuItems);
   let cartItems = JSON.parse(localStorage.getItem("cart")) || [];
   cartCounter.innerText = cartItems.length;
